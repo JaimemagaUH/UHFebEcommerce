7be39e2f08b173f390c9295cc1f27d96af1fc64d
Jaime Martínez <jaime.martinez@bootcamp-upgrade.com>
2022-05-07 14:57:40 +0200

    first commit

---
+++ src/app/pages/product-list/components/product/product.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { IProduct } from '../../models/product.models';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
+})
+export class ProductComponent implements OnInit {
+
+  @Input() public product?: IProduct;
+  @Input() public canDelete: boolean = false;
+  @Output() public delete: EventEmitter<void> = new EventEmitter();
+
+  public isSelected: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public onDelete() {
+    this.delete.emit();
+  }
+
+  public onClick() {
+    this.isSelected = !this.isSelected;
+  }
+
+}
