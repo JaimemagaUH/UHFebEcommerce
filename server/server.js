d817f8bd3ad6bd7c7395b572c49bf0bc8b8db5d2
Jaime Martínez <jaime.martinez@bootcamp-upgrade.com>
2022-05-22 11:35:32 +0200

    add server

---
+++ server/server.js
@@ -0,0 +1,47 @@
+//Imports
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+//Importamos la conexion a la db
+const { connect } = require("./api/utils/database/connect");
+
+// Express APIs
+const user = require('./api/routes/user.routes');
+
+//Ejecutamos la funcion que conecta con la db
+connect();
+
+// Express settings
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(cors());
+
+// Serve static resources
+app.use('/public', express.static('public'));
+
+app.use('/api', user)
+
+// Define PORT
+const port = process.env.PORT || 4000;
+const server = app.listen(port, () => {
+    console.log('Connected to port ' + port)
+})
+
+// Express error handling
+app.use((req, res, next) => {
+    setImmediate(() => {
+        next(new Error('Something went wrong'));
+    });
+});
+
+app.use(function (err, req, res, next) {
+    console.error(err.message);
+    if (!err.statusCode) err.statusCode = 500;
+    res.status(err.statusCode).send(err.message);
+});
